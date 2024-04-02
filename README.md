392e619bcfb0d115cc50fa34173b7ad20a0aa017
Jeeva_G <jeeva.ct20@bitsathy.ac.in>
2024-04-02 15:35:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Movie App
+
+ This app built using Next.js 14, TailwindCSS, TypeScript, and TheMovieDB API.
+
---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/Header";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
