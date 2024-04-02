b4c89f114801a457de6ba795f5594c68e5db5758
Jeeva_G <jeeva.ct20@bitsathy.ac.in>
2024-04-02 20:49:45 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
-/** @type {import('next').NextConfig} */
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  images: {
-    domains: ["image.tmdb.org"],
-  },
-};
 
-module.exports = nextConfig;
-
+images: {
+  domains: ['image.tmdb.org'],
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "**",
+    },
+  ],
+}
+  
+}
+ 
+module.exports = nextConfig
